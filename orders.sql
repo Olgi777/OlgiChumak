06c6cf1ea2919373d5ab672fc1a872d42322d233
ochum <ochumak1985@gmail.com>
2023-12-11 14:31:35 +0300

    add #commi

--- orders.sql
+++ orders.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS orders(
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE CASCADE
 );
-#comm
+#commi
